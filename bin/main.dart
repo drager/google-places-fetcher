6abdc8817b4b3cefafa6f82e995ad02cb9b25cec
Jesper Håkansson <drager@programmers.se>
2015-08-06 20:15:41 +0200

    moved main to right folder...

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,10 @@
-import 'package:google_places_fetcher/google_places_fetcher.dart';
+import '../lib/google_places_fetcher.dart';
 import 'dart:async';
 
 main(List<String> arguments) async {
   var citiesWithCoordinates = await getCoordinatesFromCityNames(cities);
   citiesWithCoordinates.forEach((city) {
-    new Future.delayed(new Duration(seconds: 10), () async {
+    new Future.delayed(new Duration(seconds: 2), () async {
       await getPlacesIn(city.latitude, city.longitude, 9000);
     });
   });
