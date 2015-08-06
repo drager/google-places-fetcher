d469e0794c90be132a0148f22ebe6c1ea4a95c15
Jesper Håkansson <drager@programmers.se>
2015-08-06 20:52:15 +0200

    fix: Wait for each async to finish, fixing HandshakeException

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,9 @@
-import '../lib/google_places_fetcher.dart';
+import 'package:google_places_fetcher/google_places_fetcher.dart';
 import 'dart:async';
 
 main(List<String> arguments) async {
   var citiesWithCoordinates = await getCoordinatesFromCityNames(cities);
-  citiesWithCoordinates.forEach((city) {
-    new Future.delayed(new Duration(seconds: 2), () async {
-      await getPlacesIn(city.latitude, city.longitude, 9000);
-    });
+  Future.forEach(citiesWithCoordinates, (city) async {
+    await getPlacesIn(city.latitude, city.longitude, 9000);
   });
 }
